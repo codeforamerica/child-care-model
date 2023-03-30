9e23a1ce649393a2c4a360cd9b74fbbbd67afc06
Cypress Borg <9101728+coltborg@users.noreply.github.com>
2023-03-30 10:51:54 -0500

    Use frozen ffl version and update conditions/actions (#27)
    Co-authored-by: Alex Gonzalez <agonzalez@codeforamerica.org>

--- src/main/java/org/formflowstartertemplate/app/submission/conditions/HasChildren.java
+++ src/main/java/org/formflowstartertemplate/app/submission/conditions/HasChildren.java
@@ -2,7 +2,9 @@ package org.formflowstartertemplate.app.submission.conditions;
 
 import formflow.library.config.submission.Condition;
 import formflow.library.data.Submission;
+import org.springframework.stereotype.Component;
 
+@Component
 public class HasChildren implements Condition {
 
   public Boolean run(Submission submission) {
