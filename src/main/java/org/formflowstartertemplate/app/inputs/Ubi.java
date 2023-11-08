826f27a97d510d699b09a5bf86f77dbc2fea38ba
Bethany Seeger <bseeger@codeforamerica.org>
2023-11-08 15:37:59 -0500

    The rest of the files.

--- src/main/java/org/formflowstartertemplate/app/inputs/Ubi.java
+++ src/main/java/org/formflowstartertemplate/app/inputs/Ubi.java
@@ -1,11 +1,11 @@
 package org.formflowstartertemplate.app.inputs;
 
-import formflow.library.data.validators.Money;
+import formflow.library.data.annotations.Money;
 import java.util.ArrayList;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 import org.springframework.web.multipart.MultipartFile;
 
